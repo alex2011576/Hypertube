e955ff34a587ad553b3f4b0e947d1058d74036f5
Ilona Shakurova <leverseau19@gmail.com>
2023-01-31 21:24:26 +0200

    Add state for logged user

---
+++ client/src/types.ts
@@ -0,0 +1,36 @@
+export type BaseUser = {
+	username: string;
+	email: string;
+	firstname: string;
+	lastname: string;
+};
+
+export type User = BaseUser & {
+	id: string;
+	passwordHash: string;
+	created_at: Date;
+};
+
+export type NewUserWithHashedPwd = BaseUser & { passwordHash: string };
+
+export type NewUser = BaseUser & { passwordPlain: string };
+
+export type LoggedUser = {
+	id: string;
+	token: string;
+	username: string;
+};
+
+export enum AlertStatus {
+	None = 'NONE',
+	Success = 'SUCCESS',
+	Error = 'ERROR'
+}
+
+export interface ImageType {
+	dataURL: string;
+}
+
+export type Images = {
+	images: ImageType[];
+};
