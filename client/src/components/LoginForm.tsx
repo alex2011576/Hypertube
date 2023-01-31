d7e691431cc801c48c0ecab1291f0d2564c3a404
Ilona Shakurova <leverseau19@gmail.com>
2023-01-31 23:00:27 +0200

    Add login & signup forms

---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,170 @@
+//prettier-ignore
+import { Avatar, Box, Button, styled, Paper, TextField, Checkbox, Grid, FormControlLabel, Container, Link, Typography } from '@mui/material';
+//prettier-ignore
+import { validatePassword, validateUsername, validateLoginForm } from '../utils/inputValidators';
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { setLoggedUser, StateContext } from '../state';
+import { AlertContext } from './AlertProvider';
+import { useField } from '../hooks/useField';
+//import userService from '../services/users';
+import loginService from '../services/login';
+
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+
+const Item = styled(Paper)(({ theme }) => ({
+	...theme.typography.body2,
+	margin: '0 auto',
+	padding: '1rem 2.5rem 4rem 2.5rem',
+	// minWidth: '320px',
+	maxWidth: '420px',
+	textAlign: 'left',
+	color: theme.palette.text.secondary
+}));
+
+const LoginForm = () => {
+	const username = useField('text', 'Username', validateUsername);
+	const password = useField('text', 'Password', validatePassword);
+
+	const [showPassword, setShow] = useState(false);
+
+	const navigate = useNavigate();
+
+	const [, dispatch] = useContext(StateContext);
+
+	const [searchParams] = useSearchParams();
+	const activationCode = searchParams.get('activate');
+	const alert = useContext(AlertContext);
+
+	// useEffect(() => {
+	// 	const activateAccount = async () => {
+	// 		if (activationCode) {
+	// 			try {
+	// 				await userService.activate(activationCode);
+	// 				navigate('/login');
+	// 				alert.success('Account activated successfully!');
+	// 			} catch (err) {
+	// 				alert.error(
+	// 					err.response?.data?.error ||
+	// 					'Unable to activate account. Please try again.'
+	// 				);
+	// 				navigate('/login');
+	// 			}
+	// 		}
+	// 	};
+	// 	activateAccount();
+	// 	// eslint-disable-next-line react-hooks/exhaustive-deps
+	// }, []);
+
+	const handleLogin = async (event: any) => {
+		event.preventDefault();
+
+		const userToLogin = {
+			username: username.value,
+			password: password.value
+		};
+
+		try {
+			const loggedInUser = await loginService.login(userToLogin);
+			alert.success(`Logged in successfully. Welcome!`);
+			dispatch(setLoggedUser(loggedInUser));
+			navigate('/');
+		} catch (err) {
+			alert.error(err.response?.data?.error || 'Unable to login. Please try again.');
+		}
+	};
+
+	return (
+		<Box sx={{ mt: 5, mb: 8 }}>
+			<Container component="main" sx={{ maxWidth: '100%' }}>
+				<Item>
+					<Box
+						sx={{
+							marginTop: 6,
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'center'
+						}}
+					>
+						<Avatar sx={{ m: 1, bgcolor: '#e3dee1' }} />
+						<Typography component="h1" variant="h5">
+							Sign in
+						</Typography>
+						<Box component="form" noValidate sx={{ mt: 1 }} onSubmit={handleLogin}>
+							<TextField
+								{...username}
+								required
+								fullWidth
+								autoFocus
+								InputLabelProps={{ shrink: true }}
+								autoComplete="username"
+							/>
+							<TextField
+								{...password}
+								required
+								fullWidth
+								type={showPassword ? 'text' : 'password'}
+								autoComplete="current-password"
+								InputLabelProps={{ shrink: true }}
+							/>
+							<Grid item xs={12} sx={{ marginLeft: '5px' }}>
+								<FormControlLabel
+									label={
+										<Box component="div" fontSize={'0.9rem'}>
+											Show password
+										</Box>
+									}
+									control={
+										<Checkbox
+											color="primary"
+											onChange={() => setShow(!showPassword)}
+											icon={
+												<VisibilityOffOutlinedIcon fontSize="small" />
+											}
+											checkedIcon={<VisibilityOutlinedIcon />}
+										/>
+									}
+								/>
+							</Grid>
+							{validateLoginForm(username.value, password.value) ? (
+								<Button
+									type="submit"
+									fullWidth
+									variant="contained"
+									sx={{ mt: 2, mb: 2 }}
+								>
+									Sign In
+								</Button>
+							) : (
+								<Button
+									type="submit"
+									fullWidth
+									disabled
+									variant="contained"
+									sx={{ mt: 2, mb: 2 }}
+								>
+									Sign In
+								</Button>
+							)}
+							<Grid container>
+								<Grid item xs>
+									<Link href="/forgot_password" variant="body2">
+										Forgot password?
+									</Link>
+								</Grid>
+								<Grid item>
+									<Link href="/signup" variant="body2">
+										Don't have an account? Sign Up
+									</Link>
+								</Grid>
+							</Grid>
+						</Box>
+					</Box>
+				</Item>
+			</Container>
+		</Box>
+	);
+};
+
+export default LoginForm;
---
+++ client/src/components/SignUpForm.tsx
@@ -0,0 +1,193 @@
+//prettier-ignore
+import { Avatar, Box, Button, TextField, FormControlLabel, Checkbox, Grid, Container, Link, Typography, Paper, styled } from '@mui/material';
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+//prettier-ignore
+import { validateEmail, validateFirstname, validateLastname, validatePassword, validateUsername, validateSignUpForm } from '../utils/inputValidators';
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AlertContext } from './AlertProvider';
+import { useField } from '../hooks/useField';
+import { NewUser } from '../types';
+// import userService from './services/users';
+
+const Item = styled(Paper)(({ theme }) => ({
+	...theme.typography.body2,
+	margin: '0 auto',
+	padding: '1rem 2.5rem 4rem 2.5rem',
+	// minWidth: '320px',
+	maxWidth: '420px',
+	textAlign: 'left',
+	color: theme.palette.text.secondary
+}));
+
+const SignUpForm = () => {
+	const firstname = useField('text', 'First Name', validateFirstname);
+	const lastname = useField('text', 'Last Name', validateLastname);
+	const username = useField('text', 'Username', validateUsername);
+	const email = useField('text', 'Email', validateEmail);
+	const password = useField('text', 'Password', validatePassword);
+
+	const [showPassword, setShow] = useState(false);
+
+	const alert = useContext(AlertContext);
+	const navigate = useNavigate();
+
+	// const addNewUser = async (newUser: NewUser) => {
+	// 	try {
+	// 		const addedUser = await userService.create(newUser);
+	// 		alert.success(
+	// 			`User ${addedUser.username} is created! Activation link is sent to email.`
+	// 		);
+	// 		navigate('/login');
+	// 	} catch (err) {
+	// 		alert.error(
+	// 			err.response?.data?.error || 'Unable to add a user. Please try again.'
+	// 		);
+	// 	}
+	// };
+
+	const submitNewUser = (event: any) => {
+		event.preventDefault();
+		const newUser: NewUser = {
+			username: username.value,
+			email: email.value,
+			passwordPlain: password.value,
+			firstname: firstname.value,
+			lastname: lastname.value
+		};
+		console.log(newUser)
+		// addNewUser(newUser);
+	};
+
+	return (
+		<Box sx={{ mt: 5, mb: 6 }}>
+			<Container component="main" sx={{ maxWidth: "100%" }}>
+				<Item>
+					<Box
+						sx={{
+							marginTop: 6,
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'center'
+						}}
+					>
+						<Avatar sx={{ m: 1, bgcolor: '#e3dee1' }} />
+						<Typography component="h1" variant="h5">
+							Sign up
+						</Typography>
+						<Box
+							component="form"
+							noValidate
+							onSubmit={submitNewUser}
+							sx={{ mt: 4 }}
+						>
+							<Grid container spacing={2}>
+								<Grid item xs={12} sm={6}>
+									<TextField
+										{...firstname}
+										required
+										fullWidth
+										autoFocus
+										style={{ margin: '0 2 0 9' }}
+										autoComplete="given-name"
+									/>
+								</Grid>
+								<Grid item xs={12} sm={6}>
+									<TextField
+										{...lastname}
+										required
+										fullWidth
+										style={{ margin: '0 2 0 9' }}
+										autoComplete="family-name"
+									/>
+								</Grid>
+								<Grid item xs={12}>
+									<TextField
+										{...username}
+										required
+										fullWidth
+										autoComplete="username"
+									/>
+								</Grid>
+								<Grid item xs={12}>
+									<TextField
+										{...email}
+										required
+										fullWidth
+										autoComplete="email"
+									/>
+								</Grid>
+								<Grid item xs={12}>
+									<TextField
+										{...password}
+										required
+										fullWidth
+										type={showPassword ? 'text' : 'password'}
+										autoComplete="new-password"
+									/>
+								</Grid>
+								<Grid item xs={12} sx={{ marginLeft: '5px' }}>
+									<FormControlLabel
+										label={
+											<Box component="div" fontSize={'0.9rem'}>
+												Show password
+											</Box>
+										}
+										control={
+											<Checkbox
+												color="primary"
+												onChange={() => setShow(!showPassword)}
+												icon={
+													<VisibilityOffOutlinedIcon
+														fontSize={'small'}
+													/>
+												}
+												checkedIcon={<VisibilityOutlinedIcon />}
+											/>
+										}
+									/>
+								</Grid>
+							</Grid>
+							{validateSignUpForm(
+								username.value,
+								email.value,
+								password.value,
+								firstname.value,
+								lastname.value
+							) ? (
+								<Button
+									type="submit"
+									fullWidth
+									variant="contained"
+									sx={{ mt: 2, mb: 2 }}
+								>
+									Sign Up
+								</Button>
+							) : (
+								<Button
+									type="submit"
+									fullWidth
+									disabled
+									variant="contained"
+									sx={{ mt: 2, mb: 2 }}
+								>
+									Sign Up
+								</Button>
+							)}
+							<Grid container justifyContent="flex-end">
+								<Grid item>
+									<Link href="/login" variant="body2">
+										Already have an account? Sign in
+									</Link>
+								</Grid>
+							</Grid>
+						</Box>
+					</Box>
+				</Item>
+			</Container>
+		</Box>
+	);
+};
+
+export default SignUpForm;
