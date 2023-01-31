c5a8198796207f88ad36e8a777dc04d0a55d177d
Ilona Shakurova <leverseau19@gmail.com>
2023-01-31 21:25:00 +0200

    Add errors utils

---
+++ client/src/utils/errors.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export class AppError extends Error {
+	constructor(message: string) {
+		super(message);
+		this.name = 'AppError';
+	}
+}
+
+export class AuthError extends Error {
+	constructor(message: string) {
+		super(message);
+		this.name = 'AuthError';
+	}
+}
+
+export const handleAxiosError = (err: unknown): never => {
+	if (axios.isAxiosError(err)) {
+		if (err.response) {
+			if (err.response.status === 401) {
+				throw new AuthError('Session expired, please login again.');
+			}
+			if (err.response.data && err.response.data.error) {
+				throw new AppError(err.response.data.error);
+			}
+		}
+	}
+	throw err;
+};
