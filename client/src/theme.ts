85e24933b0a33106f67600ea14fe287e775c799f
Ilona Shakurova <leverseau19@gmail.com>
2023-01-31 21:22:55 +0200

    Add initial theme

---
+++ client/src/theme.ts
@@ -0,0 +1,41 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+	typography: {
+		fontFamily: 'DM Sans, sans-serif',
+		fontWeightLight: 300,
+		fontSize: 14
+	},
+	palette: {
+		primary: {
+			main: '#001489',
+			contrastText: '#ffffff'
+		},
+		secondary: {
+			main: '#ffffff',
+			contrastText: '#001489'
+		},
+		text: {
+			primary: '#414141',
+			secondary: '#9e9e9e',
+			disabled: '#bdbdbd'
+		}
+	},
+	components: {
+		MuiAppBar: {
+			styleOverrides: {
+				root: {
+					position: 'fixed',
+					display: 'flex',
+					justifyContent: 'space-between',
+					borderBottom: '1px solid #b2b2b245',
+					borderRadius: 0,
+					maxWidth: '100%',
+					boxShadow: 'none'
+				}
+			}
+		}
+	}
+});
+
+export default theme;
