b7314c7e8ab18e332ad03986e821f5d09e127286
Ilona Shakurova <leverseau19@gmail.com>
2023-02-03 00:01:56 +0200

    Add service to authenticate with 42 strategy / client

--- client/src/services/login.tsx
+++ client/src/services/login.tsx
@@ -8,5 +8,13 @@ const login = async (credentials: { username: string; password: string }) => {
 	return response.data;
 };
 
-const moduleExports = { login };
+const auth42 = async (code: string, state: string) => {
+	const response = await axios.get(
+		`${apiBaseUrl}/login/42/callback?code=${code}&state=${state}`
+	);
+	localStorage.setItem('loggedUser', JSON.stringify(response.data));
+	return response.data;
+};
+
+const moduleExports = { login, auth42 };
 export default moduleExports;
