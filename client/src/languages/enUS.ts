cff3340c1e4c5dc6cddbfa076befa50bf13747fc
Cong Nguyen <kaom.n.92@gmail.com>
2023-02-03 21:16:47 +0200

    Adding imports for english, swedish and russian translations

---
+++ client/src/languages/enUS.ts
@@ -0,0 +1,42 @@
+export const enUS = {
+	main: "Main page",
+
+	// Text Fields
+	textFieldUsername: "Username",
+	textFieldPassword: "Password",
+	textFieldEmail: "Email",
+	textFieldFirstName: "First Name",
+	textFieldLastName: "Last Name",
+	passwordTooltip: "Password should be at least 8 characters long, contain at least one uppercase and lowercase letter, number and symbols",
+
+	// Checkbox
+	showPassword: "Show password",
+
+	// Login Form
+	orDivider: "OR",
+	titleLogin: "LOG IN",
+	troubleLoggingIn: "Forgot password?",
+	buttonLogin: "LOG IN",
+	newToLink: "New to Hypertube?",
+	signupNow: "Sign up now!",
+	logInWith42: "Log in with 42",
+
+	// Signup Form
+	titleSignup: "SIGN UP",
+	titleResetPassword: "RESET PASSWORD",
+	signupButton: "SIGN UP",
+	signupAlready: "Already a member?",
+	signupLogin: "Log in",
+
+	// Forgot Password Form
+	forgotPasswordInfoText: "Enter your email address and we'll send you a link to reset your password.",
+	forgotPasswordButton: "SEND",
+	forgotPasswordBackLogin: "Back to sign in ",
+	forgotPasswordSignup: "Sign Up",
+	passwordResetSignup: "Don't have an account? Sign Up",
+	passwordResetSubmitButton: "SUBMIT",
+
+	// Navbar
+	navbarLogin: "LOGIN",
+	navbarSignup: "SIGN UP",
+}
---
+++ client/src/languages/seSWE.ts
@@ -0,0 +1,42 @@
+export const seSWE = {
+	main: "Huvudsida",
+
+	// Text Fields
+	textFieldUsername: "Användarnamn",
+	textFieldPassword: "Lösenord",
+	textFieldEmail: "E-post",
+	textFieldFirstName: "Förnamn",
+	textFieldLastName: "Efternamn",
+	passwordTooltip: "Lösenordet ska vara minst 8 tecken lång, innehålla minst en stor och liten bokstav, siffror och symboler",
+
+	// Checkbox
+	showPassword: "Visa lösenord",
+
+	// Login Form
+	orDivider: "ELLER",
+	titleLogin: "LOGGA IN",
+	troubleLoggingIn: "Glömt lösenordet?",
+	buttonLogin: "LOGGA IN",
+	newToLink: "Ny på Hypertube?",
+	signupNow: "Registrera dig nu!",
+	logInWith42 : "Logga in med 42",
+
+	// Signup Form
+	titleSignup: "BLI MEDLEM",
+	titleResetPassword: "ÅTERSTÄLL LÖSENORD",
+	signupButton: "BLI MEDLEM",
+	signupAlready: "Redan en medlem?",
+	signupLogin: "Logga in",
+
+	// Forgot Password Form
+	forgotPasswordInfoText: "Ange din e-postadress så skickar vi dig en länk för att återställa ditt lösenord.",
+	forgotPasswordButton: "SKICKA",
+	forgotPasswordBackLogin: "Tillbaka till inloggning",
+	forgotPasswordSignup: "Bli medlem",
+	passwordResetSignup: "Don't have an account? Sign Up",
+	passwordResetSubmitButton: "SPARA",
+
+	// Navbar
+	navbarLogin: "Logga in",
+	navbarSignup: "Bli medlem",
+};
