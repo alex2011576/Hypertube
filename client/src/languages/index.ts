283e2ca4dd856f2b5a7b15a40857abde6033cf65
Cong Nguyen <kaom.n.92@gmail.com>
2023-02-03 21:15:30 +0200

    Added Swedish, and changed from .json to importing from .js file instead for comenting

--- client/src/languages/index.ts
+++ client/src/languages/index.ts
@@ -1,11 +1,13 @@
-import enUS from './enUS.json';
+import { enUS } from './enUS';
 import ruRU from './ruRU.json';
+import { seSWE } from './seSWE';
 
-export const dictionaryList = { enUS, ruRU };
+export const dictionaryList = { enUS, ruRU, seSWE };
 
 export const languageOptions = {
 	enUS: '🇬🇧',
-	ruRU: '🇷🇺'
+	ruRU: '🇷🇺',
+	seSWE: '🇸🇪'
 };
 
 // https://emojipedia.org/
