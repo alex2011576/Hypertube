a23e7917800417676480c4a0a75ec97f683f5286
Ilona Shakurova <leverseau19@gmail.com>
2023-02-06 19:07:25 +0200

    Add service call to forgot and reset pwd

--- client/src/components/ForgotPassword/ForgotPasswordForm.tsx
+++ client/src/components/ForgotPassword/ForgotPasswordForm.tsx
@@ -1,33 +1,32 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 //prettier-ignore
-import { Avatar, Box, Button, TextField, Grid, Container, Link, Typography,  } from '@mui/material';
+import { Box, Button, TextField, Grid, Container, Link, Typography,  } from '@mui/material';
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useField } from '../../hooks/useField';
 import { validateEmail } from '../../utils/inputValidators';
 import { AlertContext } from '../AlertProvider';
 import Text from '../Text';
-// import userService from '../../services/users';
+import userService from '../../services/users';
 
 const ForgotPasswordForm = () => {
-	const email = useField('text', <Text tid='textFieldEmail' />, validateEmail);
+	const email = useField('text', <Text tid="textFieldEmail" />, validateEmail);
 	const alert = useContext(AlertContext);
 	const navigate = useNavigate();
 
-	// const handleForgotPwdRequest = async (event: any) => {
-	// 	event.preventDefault();
+	const handleForgotPwdRequest = async (event: any) => {
+		event.preventDefault();
 
-	// 	try {
-	// 		await userService.requestPasswordReset(email.value);
-	// 		alert.success('Reset link sent! Please check your inbox.');
-	// 		navigate('/login');
-	// 	} catch (err) {
-	// 		alert.error(
-	// 			err.response?.data?.error || 'Unable to send a link. Please try again.'
-	// 		);
-	// 		navigate('/forgot_password');
-	// 	}
-	// };
+		try {
+			await userService.requestPasswordReset(email.value);
+			alert.success('Reset link sent! Please check your inbox.');
+			navigate('/login');
+		} catch (err) {
+			alert.error(
+				err.response?.data?.error || 'Unable to send a link. Please try again.'
+			);
+			navigate('/forgot_password');
+		}
+	};
 
 	return (
 		<Container component="main" sx={{ maxWidth: '100%', mt: 2, mb: 6 }}>
@@ -39,14 +38,14 @@ const ForgotPasswordForm = () => {
 				}}
 			>
 				<Typography mb={3} textAlign="center">
-					<Text tid='forgotPasswordInfoText' />
+					<Text tid="forgotPasswordInfoText" />
 				</Typography>
 
 				<Box
 					component="form"
 					noValidate
-					sx={{display: 'flex', flexDirection: 'column', width: '80%'}}
-					// onSubmit={handleForgotPwdRequest}
+					sx={{ display: 'flex', flexDirection: 'column', width: '80%' }}
+					onSubmit={handleForgotPwdRequest}
 				>
 					<TextField
 						{...email}
@@ -62,17 +61,17 @@ const ForgotPasswordForm = () => {
 						variant="contained"
 						sx={{ mt: 2, mb: 2 }}
 					>
-						<Text tid='forgotPasswordButton' />
+						<Text tid="forgotPasswordButton" />
 					</Button>
 					<Grid container>
 						<Grid item xs>
 							<Link href="/login" variant="body2">
-								<Text tid='forgotPasswordBackLogin' />
+								<Text tid="forgotPasswordBackLogin" />
 							</Link>
 						</Grid>
 						<Grid item>
 							<Link href="/signup" variant="body2">
-							<Text tid='forgotPasswordSignup' />
+								<Text tid="forgotPasswordSignup" />
 							</Link>
 						</Grid>
 					</Grid>
