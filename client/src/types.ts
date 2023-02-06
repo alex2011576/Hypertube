8d6f2c5265406322e29ac71d63f7b29b07c9e676
Cong Nguyen <kaom.n.92@gmail.com>
2023-02-06 14:32:22 +0200

    Added NewUserData type

--- client/src/types.ts
+++ client/src/types.ts
@@ -38,4 +38,11 @@ export type UserData = {
 	language: string | undefined;
 };
 
+export type NewUserData = {
+	username: string;
+	firstname: string;
+	lastname: string;
+	language: string | undefined;
+}
+
 export type LanguageOption = 'enUS' | 'ruRU' | 'seSWE';
