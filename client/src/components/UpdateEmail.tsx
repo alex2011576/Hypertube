9f9ed7215edb3e84fd5ffeea91095d0d8b6b79a8
Ilona Shakurova <leverseau19@gmail.com>
2023-02-13 16:05:53 +0200

    Add updateEmail component

---
+++ client/src/components/UpdateEmail.tsx
@@ -0,0 +1,35 @@
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import { useSearchParams } from 'react-router-dom';
+import { AlertContext } from './AlertProvider';
+import profileService from '../services/profile';
+
+const UpdateEmail = () => {
+	const { success: successCallback, error: errorCallback } = useContext(AlertContext);
+	const [searchParams] = useSearchParams();
+	const updateToken = searchParams.get('update');
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const validateUpdateToken = async () => {
+			if (updateToken) {
+				try {
+					await profileService.updateEmailByToken(updateToken);
+					successCallback('Email was successfully updated!');
+					navigate('/');
+				} catch (err) {
+					errorCallback('Invalid update link. Please try again.');
+					navigate('/');
+				}
+			} else {
+				navigate('/');
+			}
+		};
+		validateUpdateToken();
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	return <></>;
+};
+
+export default UpdateEmail;
