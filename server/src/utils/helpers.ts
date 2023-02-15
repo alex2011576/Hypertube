2e455c56fe666d1617a13753b07e77534b25d45d
Aleksei Shatalov <84226106+alex2011576@users.noreply.github.com>
2023-02-15 15:27:18 +0200

    helper functions for streaming services

--- server/src/utils/helpers.ts
+++ server/src/utils/helpers.ts
@@ -46,3 +46,19 @@ export const imgUrlToBase64 = async (url: string | undefined | null) => {
 		return undefined;
 	}
 };
+
+export const getMagnetLink = (filmTitle: string, hash: string) => {
+	return `magnet:?xt=urn:btih:${hash}&dn=${filmTitle.split(' ').join('+')}`;
+};
+
+export function convertBytes(bytes: number): string {
+	if (bytes < 1024) {
+		return bytes + ' B';
+	} else if (bytes < 1048576) {
+		return (bytes / 1024).toFixed(2) + ' KB';
+	} else if (bytes < 1073741824) {
+		return (bytes / 1048576).toFixed(2) + ' MB';
+	} else {
+		return (bytes / 1073741824).toFixed(2) + ' GB';
+	}
+}
