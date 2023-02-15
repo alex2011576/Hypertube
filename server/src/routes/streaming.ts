bc510f0dd8ae9d696d3f164e2afed18d806e871a
Aleksei Shatalov <84226106+alex2011576@users.noreply.github.com>
2023-02-15 15:26:02 +0200

    router for streaming

---
+++ server/src/routes/streaming.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import { AppError } from '../errors';
+import { getStreamStatus } from '../services/streaming';
+import { CustomRequest } from '../types';
+import { sessionExtractor } from '../utils/middleware';
+import { isIMDB, isQuality } from '../validators/basicTypeValidators';
+
+const router = express.Router();
+
+router.get(
+	'/:imdb/:quality',
+    sessionExtractor,
+	asyncHandler(async (req: CustomRequest, res) => {
+        if (!req.session || !req.session.userId) {
+			throw new AppError(`User is not logged in!`, 400);
+		}
+		const imdb = req.params.imdb;
+		const quality = req.params.quality;
+        if (!isIMDB(imdb)) throw new AppError(`Invalid imdb id`, 400);
+        if (!isQuality(quality)) throw new AppError(`Invalid quality parameter`, 400);
+
+		const streamStatus = await getStreamStatus(imdb, quality);
+		console.log(streamStatus);
+		
+        res.status(200).send(streamStatus);
+	})
+);
+
+export default router;
