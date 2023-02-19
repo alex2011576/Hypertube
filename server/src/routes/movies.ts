229e3d042549033d9fb1a5915a2d17f4aa3913b7
Cong Nguyen <kaom.n.92@gmail.com>
2023-02-19 17:36:14 +0200

    added routes for fetching reviews and adding review

--- server/src/routes/movies.ts
+++ server/src/routes/movies.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import { isStringRepresentedInteger } from '../validators/basicTypeValidators';
+import { isStringRepresentedInteger, isNumber } from '../validators/basicTypeValidators';
 import { CustomRequest, SearchQuerySchema } from '../types';
 import { AppError, ValidationError } from '../errors';
 import { sessionExtractor } from '../utils/middleware';
 import { getMovies } from '../services/movies';
 import { getMovieData } from '../services/movie';
 import { isRight } from 'fp-ts/lib/Either';
+import { getMovieReviews, addMovieReview } from '../services/movies';
+import { parseNewReviewPayload } from '../validators/reviewPayloadValidators';
 
 const router = express.Router();
 
@@ -43,8 +45,44 @@ router.get(
 		if (!result) {
 			throw new AppError(`movieMovieNotFound`, 400);
 		}
-
 		res.status(200).json(result);
+	})
+);
+
+router.get(
+	'/:id/reviews',
+	sessionExtractor,
+	asyncHandler(async (req: CustomRequest, res) => {
+		if (!req.session || !req.session.userId) {
+			throw new AppError(`moviesUserNotLoggedIn`, 400);
+		}
+		const ytsMovieId = req.params.id;
+		const page = req.query.page;
+		if (!isStringRepresentedInteger(ytsMovieId)) {
+			throw new AppError(`movieReviewsNotFound`, 400);
+		}
+		if (!isStringRepresentedInteger(page)) {
+			throw new AppError(`movieReviewsPageInvalidType`, 400);
+		}
+		if (!isNumber(Number(page))) {
+			throw new AppError(`movieReviewsPageInvalidType`, 400);
+		}
+
+		const result = await getMovieReviews({ytsMovieId: ytsMovieId, page: Number(page)});
+		res.status(200).json(result);
+	})
+);
+
+router.post(
+	'/:id/reviews',
+	sessionExtractor,
+	asyncHandler(async (req: CustomRequest, res) => {
+		if (!req.session || !req.session.userId) {
+			throw new AppError(`moviesUserNotLoggedIn`, 400);
+		}
+		const newReview = parseNewReviewPayload({text: req.body.text, rating: req.body.rating, userId: req.session.userId, ytsId: req.params.id});
+		await addMovieReview(newReview);
+		res.status(201).end();
 	})
 );
 
