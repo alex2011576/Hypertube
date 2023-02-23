3e6fde6caa6db3ff3d7799adde1ae67cfc5a5838
Ilona Shakurova <leverseau19@gmail.com>
2023-02-23 13:22:49 +0200

    Remove unused function getAge

--- server/src/utils/helpers.ts
+++ server/src/utils/helpers.ts
@@ -22,18 +22,6 @@ export const checkIfDuplicatesExist = (arr: string[]) => {
 	return new Set(arr).size !== arr.length;
 };
 
-//might fail when offfset back by local if not same timezone as front
-export const getAge = (dateString: string): number => {
-	const today = new Date();
-	const birthDate = new Date(dateString);
-	let age = today.getFullYear() - birthDate.getFullYear();
-	const m = today.getMonth() - birthDate.getMonth();
-	if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-		age--;
-	}
-	return age;
-};
-
 export const assertNever = (value: string): never => {
 	throw new Error(`Unhandled discriminated union member: ${value}`);
 };
